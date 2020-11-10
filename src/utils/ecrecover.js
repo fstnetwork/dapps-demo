e309ec8e8574a165da98d0b0a148d8a8858d9a2c
root <root@DESKTOP-JEUN4JN.localdomain>
2020-11-10 16:14:40 +0800

    add comments

--- src/utils/ecrecover.js
+++ src/utils/ecrecover.js
@@ -4,6 +4,7 @@ const ethers = require("ethers");
 
 const utils = ethers.utils;
 
+// calculate the hash of the msg via https://eips.ethereum.org/EIPS/eip-191
 const hash_hello_world = utils.hashMessage("hello world");
 const hash_你好世界 = utils.hashMessage("你好世界");
 
@@ -13,7 +14,9 @@ consola.info(hash_你好世界);
 // do not use in other places, only for testing
 const pk = "0x3dbef63d01cb65ccf1c2d142e15cfbdacb83e3bd241eabd540843a1dbccc2822";
 
+// create the signing key instance
 const signing_key = new utils.SigningKey(pk);
 
+// sign the hashes to obtain the digital signature
 consola.info(signing_key.signDigest(hash_hello_world));
 consola.info(signing_key.signDigest(hash_你好世界));
